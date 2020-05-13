8bd6b981a148a6a66e82dd3f7a5488d5d009f964
TOtto-EclipseIR <58695819+TOtto-EclipseIR@users.noreply.github.com>
2020-05-13 12:47:59 -0700

    Start CommandLine branch [#24]

--- VERSION.h
+++ VERSION.h
@@ -3,12 +3,12 @@
 #define VER_MAJOR 2
 #define VER_TRUNK "develop"
 #define EIRC2_VER_MAJOR 2
-#define EIRC2_VER_MINOR 6
-#define EIRC2_VER_RELEASE 0xA0
-#define EIRC2_VER_BRANCH 0
-#define EIRC2_VER_STRING "v2.06-Alpha"
-#define EIRC2_VER_TRUNKNAME "current"
-#define EIRC2_VER_APPVER EIRC2_VER_STRING
+#define EIRC2_VER_MINOR 7
+#define EIRC2_VER_RELEASE 1
+#define EIRC2_VER_BRANCH 24
+#define EIRC2_VER_STRING "v2.07A+0024"
+#define EIRC2_VER_TRUNKNAME "develops"
+#define EIRC2_VER_APPVER EIRC2_VER_STRINGs
 #ifdef NDEBUG
 #define EIRC2_VER_ORGNAME "EclipseIR"
 #else
--- ourbld.pri
+++
@@ -1,8 +0,0 @@
-# file: {EIRC2repo}./ourbld.pri
-
-OURBLD = "{???}"
-windows:OURBLD = "windows"
-win32:OURBLD = "win32"
-win64:OURBLD = "win64"
-
-#message(OURBLD = $$OURBLD)
