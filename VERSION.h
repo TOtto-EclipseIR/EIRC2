8bf6e605326c7351f1141fadd701faa7f4f08684
TOtto-EclipseIR <58695819+TOtto-EclipseIR@users.noreply.github.com>
2020-05-13 12:51:32 -0700

    Start v2.07B+0023 for MS03 [#23]

--- VERSION.h
+++ VERSION.h
@@ -3,11 +3,13 @@
 #define VER_MAJOR 2
 #define VER_TRUNK "develop"
 #define EIRC2_VER_MAJOR 2
-#define EIRC2_VER_MINOR 6
-#define EIRC2_VER_RELEASE 0xA0
-#define EIRC2_VER_BRANCH 0
-#define EIRC2_VER_STRING "v2.06-Alpha"
-#define EIRC2_VER_TRUNKNAME "current"
+#define EIRC2_VER_MINOR 7
+#define EIRC2_VER_RELEASE 2
+#define EIRC2_VER_BRANCH 23
+#define EIRC2_VER_STRING "v2.07B+0023"
+#define EIRC2_VER_TRUNKNAME "develop"
+#define EIRC2_VER_BRANCHNAME "dev+0023"
+
 #define EIRC2_VER_APPVER EIRC2_VER_STRING
 #ifdef NDEBUG
 #define EIRC2_VER_ORGNAME "EclipseIR"
