1f78c1e085348f466debe17871ecff098d3a6c30
TOtto-EclipseIR <58695819+TOtto-EclipseIR@users.noreply.github.com>
2020-05-28 19:58:40 -0700

    Clean build w/o eirHaar library. [#23]

--- src/version.h
+++ src/version.h
@@ -1,15 +1,12 @@
 // file: ./version.h
 
-#define VER_MAJOR 2
-#define VER_TRUNK "develop"
 #define EIRC2_VER_MAJOR 2
 #define EIRC2_VER_MINOR 7
-#define EIRC2_VER_BRANCH 23
-#define EIRC2_VER_STRING "v2.07B+23"
+#define EIRC2_VER_STRING "v2.07B"
 #define EIRC2_VER_RELEASE 2
-#define EIRC2_VER_BRANCH 23
+#define EIRC2_VER_BRANCH 0
 #define EIRC2_VER_TRUNKNAME "develop"
-#define EIRC2_VER_BRANCHNAME "MS03"
+#define EIRC2_VER_BRANCHNAME ""
 #define EIRC2_VER_APPVER EIRC2_VER_STRING
 #ifdef NDEBUG
 #define EIRC2_VER_ORGNAME "EclipseIR"
