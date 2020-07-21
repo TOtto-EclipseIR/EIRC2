94286020283cbe3e270d3f32a0b3cf4f799b9812
TOtto-EclipseIR <58695819+TOtto-EclipseIR@users.noreply.github.com>
2020-07-21 12:36:55 -0700

    start ResourceManager class [#43]

---
+++ src/libs/eirResource/ResourceManager.cpp
@@ -0,0 +1,6 @@
+#include "ResourceManager.h"
+
+ResourceManager::ResourceManager(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ src/libs/eirResource/ResourceManager.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <QObject>
+
+class ResourceManager : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ResourceManager(QObject *parent = nullptr);
+
+signals:
+
+};
+
