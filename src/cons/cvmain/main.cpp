5a69a068f8d23de2ea833fd97eba72fc7fbc2f95
Tony Otto <58695819+TOtto-EclipseIR@users.noreply.github.com>
2020-08-22 10:00:39 -0700

    Detecting rectangles!

--- src/cons/cvmain/main.cpp
+++ src/cons/cvmain/main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
     if (face_cascade.load("D:/bin/detectors/v4/haarcascade_frontalcatface_extended.Xml"))
     {
         std::cout << "face_cascade.load() " << "D:/bin/detectors/v4/haarcascade_frontalcatface_extended.Xml" << " loaded" << std::endl;
-        std::cout << "face_cascade.empty() " << (face_cascade.empty() ? "true!" : "FALSE") << std::endl;;
+        std::cout << "face_cascade.empty() " << (face_cascade.empty() ? "true!" : "FALSE") << std::endl;
     }
     else
     {
@@ -45,12 +45,35 @@ int main(int argc, char *argv[])
 
     cv::Mat inputMat = cv::imread("D:/INDIface/INDIin/console/hide/18yoTwins.jpg", cv::IMREAD_COLOR);
     matInfo(inputMat);
-    std::cout << "inputMat is " << (isValid(inputMat) ? "valid" : "INVALID");
+    std::cout << "inputMat is " << (isValid(inputMat) ? "valid" : "INVALID") << std::endl;
     //cv::namedWindow("", cv::WINDOW_NORMAL);
     cv::imshow("inputMat", inputMat);
     cv::waitKey();
 
-//    char dummy;
-  //  std::cin >> dummy;
+    cv::Mat detectMat;
+    cv::cvtColor(inputMat, detectMat, cv::COLOR_BGR2GRAY);
+    matInfo(detectMat);
+    cv::imshow("detectMat", detectMat);
+    cv::waitKey();
+
+    std::vector<cv::Rect> faceRects;
+    face_cascade.detectMultiScale(detectMat, faceRects, 1.01, 1); // , cv::CASCADE_DO_ROUGH_SEARCH);
+    std::cout << faceRects.size() << " Face Rectangles Detected" << std::endl;
+
+    cv::Mat outputMat = inputMat;
+    for (int x = 0; x < faceRects.size(); ++x)
+    {
+        cv::Rect rc = faceRects[x];
+        cv::circle(outputMat,
+                   cv::Point(rc.x + rc.width/2,
+                             rc.y + rc.height/2),
+                   (rc.width + rc.height) / 2,
+                   cv::Scalar(255, 255, 0),
+                   3);
+    }
+    matInfo(outputMat);
+    cv::imshow("outputMat", outputMat);
+    cv::waitKey();
+
     return 0;
 }
